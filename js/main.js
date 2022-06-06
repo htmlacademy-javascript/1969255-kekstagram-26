09fd1f110f6971af23faf44123f069e05065f71a
Alex <bebko07@gmail.com>
2022-06-06 14:44:33 +0300

    Fixes bugs

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 //функцию по поиска случайного числа нашел по ссылке https://myrusakov.ru/js-random-numbers.html
 function getRandomInRange(minValue, maxValue) {
   if (minValue < 0 || maxValue < 0) {
-    // не придумал, что можно сделать вместо console.log для вывода ошибки
-    // eslint-disable-next-line no-console
-    return console.log('Значения должны быть положительными');
+    return 0;
   }
   if (maxValue <= minValue) {
-    // изначально менял минимальное и максимальное значение местами создавая дополнительную переменную,
-    // но решил заменить значения сразу в выводе.
-    // в таком случае в данном диапазоне все равно будет выдаваться случайное значение
     return Math.floor(Math.random() * (minValue - maxValue + 1)) + maxValue;
   }
   return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
@@ -18,10 +13,7 @@ getRandomInRange(0, 10);
 
 // Функция для проверки максимальной длины строки
 function checkStringLength(stringToTest, maxStringLength) {
-  if (stringToTest.length <= maxStringLength) {
-    return true;
-  }
-  return false;
+  return stringToTest.length <= maxStringLength;
 }
 
 checkStringLength('', 140);

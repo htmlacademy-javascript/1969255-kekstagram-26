6cb1b34587cd515c2381746027ec0dd5e2a4d738
Alex <bebko07@gmail.com>
2022-07-12 23:34:40 +0300

    displays comments list

--- js/forms.js
+++ js/forms.js
@@ -1,4 +1,4 @@
-import { checkStringLength } from './util.js';
+import { checkStringLength, isEscapeKey } from './util.js';
 
 const VALID_HASHTAG = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 const MAX_NUMBER_OF_HASHTAGS = 5;
@@ -13,19 +13,8 @@ const uploadFormElement = document.querySelector('#upload-select-image');
 const commentTextElement = uploadFormElement.querySelector('.text__description');
 const hashtagElement = uploadFormElement.querySelector('.text__hashtags');
 
-
-uploadFileElement.addEventListener('change', () => {
-  uploadImageModalElement.classList.remove('hidden');
-  bodyElement.classList.add('modal-open');
-});
-
-closeUploadImageModalElement.addEventListener('click', () => {
-  uploadImageModalElement.classList.add('hidden');
-  bodyElement.classList.remove('modal-open');
-});
-
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape') {
+function onUploadModalEscapeKeydown (event) {
+  if (isEscapeKey(event)) {
     if (commentTextElement === document.activeElement || hashtagElement === document.activeElement) {
       return bodyElement.classList.add('modal-open');
     }
@@ -33,7 +22,31 @@ document.addEventListener('keydown', (event) => {
     bodyElement.classList.remove('modal-open');
     uploadFileElement.value = '';
     commentTextElement.value = '';
+    closeUploadImageModal();
   }
+}
+
+function openUploadImageModal () {
+  uploadImageModalElement.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+
+  document.addEventListener('keydown', onUploadModalEscapeKeydown);
+}
+
+function closeUploadImageModal () {
+  uploadImageModalElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onUploadModalEscapeKeydown);
+}
+
+uploadFileElement.addEventListener('change', () => {
+  openUploadImageModal();
+});
+
+closeUploadImageModalElement.addEventListener('click', () => {
+  closeUploadImageModal();
+
 });
 
 
@@ -46,32 +59,32 @@ const pristine = new Pristine(uploadFormElement, {
   errorTextClass: 'form__error'
 });
 
-function createHashtagsArray() {
-  return hashtagElement.value.toLowerCase().trim().split(' ');
+function parseHashtags() {
+  return hashtagElement.value.toLowerCase().trim().split(' ').filter((x) => x.length > 0);
 }
 
 
 function checkHashtagValidity() {
-  const hashtagsArray = createHashtagsArray();
-  for (let i = 0; i < hashtagsArray.length; i++) {
-    if (hashtagsArray[i] === '') {
-      return true;
-    } else if (!VALID_HASHTAG.test(hashtagsArray[i])) {
+  const hashtags = parseHashtags();
+
+  for (let i = 0; i < hashtags.length; i++) {
+    if (!VALID_HASHTAG.test(hashtags[i])) {
       return false;
     }
   }
+
   return true;
 }
 
 function checkHashtagLength() {
-  const hashtagsArray = createHashtagsArray();
-  return hashtagsArray.length <= MAX_NUMBER_OF_HASHTAGS;
+  const hashtags = parseHashtags();
+  return hashtags.length <= MAX_NUMBER_OF_HASHTAGS;
 }
 
 function checkHashtagsRepeats() {
-  const hashtagsArray = createHashtagsArray();
-  const uniqueHashtagsArray = [...new Set(hashtagsArray)];
-  return hashtagsArray.length === uniqueHashtagsArray.length;
+  const hashtags = parseHashtags();
+  const uniqueHashtags = [...new Set(hashtags)];
+  return hashtags.length === uniqueHashtags.length;
 }
 
 
